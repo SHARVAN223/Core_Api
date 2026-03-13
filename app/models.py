3a6d6b8283e27ec85c0f79b54014b40519669428
SHARVAN223 <sharvan70458@gmail.com>
2026-03-13 23:05:21 +0530

    core api

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Api(models.Model):
+    Name = models.CharField(max_length=50)
+    Age = models.IntegerField()
+    City = models.CharField(max_length=50)
+    Contact = models.CharField(max_length=15)
+    
