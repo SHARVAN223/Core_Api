da4d53e0ca0f165b4d28bc2781d4b5653ec714b1
SHARVAN223 <sharvan70458@gmail.com>
2026-03-14 21:48:48 +0530

    api2

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from app.models import Api
 # Create your views here.
+import json
+
+def all_data(req):
+    data = Api.objects.all()
+    print(data.values())
+    p_data = list(data.values())
+    print(p_data)
+    j_data = json.dumps(p_data)
+    
+    print(j_data)
+    return HttpResponse(j_data,content_type='application/json')
